3aaa51ca3886c8bf9bc765854c078d14d9b91f9a
Jeremiah Garza <jag_tt@yahoo.com>
2019-10-20 23:00:07 -0500

    images showing

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -52,6 +52,9 @@ class Search extends Component {
           <MenuItem value={50} primaryText="50" />
         </SelectField>
         <br />
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
